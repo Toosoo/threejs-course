4b0696669c9d7b57d4c645d56671dae2235bfc16
toso <hello@iamtoso.com>
2026-03-07 22:27:31 +0200

    3- basics 2

--- src/main.js
+++ src/main.js
@@ -1,14 +1,38 @@
 import * as THREE from 'three';
 import './style.css';
+import gsap from 'gsap';
 
 const canvasElement = document.getElementById('canvas')
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 'blue' });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const group1 = new THREE.Group()
+scene.add(group1);
+
+group1.position.x = 1
+
+const cube1 = new THREE.Mesh(
+new THREE.BoxGeometry(1, 1, 1),
+new THREE.MeshBasicMaterial({ color: 'blue' }));
+
+const cube2 = new THREE.Mesh(
+new THREE.BoxGeometry(1, 1, 1),
+new THREE.MeshBasicMaterial({ color: 'red' }));
+
+
+gsap.to(cube1.position,{
+    y:2,
+    duration:1,
+    repeat:-1,
+    yoyo:true
+})
+
+group1.add(cube1);
+group1.add(cube2);
+
+
+cube2.position.x = -2
+
 
 const camera = new THREE.PerspectiveCamera(75, 900/600);
 camera.position.z = 5;
@@ -19,5 +43,10 @@ const renderer = new THREE.WebGLRenderer({
 });
 
 renderer.setSize(900, 600);
-renderer.render(scene, camera);
 
+const animate = ()=> {
+    renderer.render(scene, camera);
+    
+    window.requestAnimationFrame(animate)
+}
+animate()
