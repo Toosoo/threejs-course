3a7891d0c9f034c3817849c07cd30f7f9ae5f169
toso <hello@iamtoso.com>
2026-03-11 10:52:21 +0200

    init

--- src/main.js
+++ src/main.js
@@ -4,37 +4,62 @@ import gsap from 'gsap';
 
 const canvasElement = document.getElementById('canvas')
 
+
 const scene = new THREE.Scene();
 
-const group1 = new THREE.Group()
-scene.add(group1);
 
-group1.position.x = 1
+// *************** old method 
 
-const cube1 = new THREE.Mesh(
-new THREE.BoxGeometry(1, 1, 1),
-new THREE.MeshBasicMaterial({ color: 'blue' }));
+// const sizes = {
+//     width:window.innerWidth,
+//     height:window.innerHeight
+// }
 
-const cube2 = new THREE.Mesh(
+// window.addEventListener('resize',()=>{
+//     sizes.width = window.innerWidth
+//     sizes.height = window.innerHeight
+
+//     camera.aspect = sizes.width/sizes.height
+//     camera.updateProjectionMatrix()
+
+//     renderer.setSize(sizes.width,sizes.height);
+//     renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+
+// })
+
+
+// *************** new method 
+
+function resizeRendererToDisplaySize(renderer,maxPixelCount = 3480 * 2160) {
+    const canvas = renderer.domElement
+    const pixelRatio = window.devicePixelRatio
+
+    let width = Math.floor(canvas.clientWidth * pixelRatio)
+    let height = Math.floor(canvas.clientHeight * pixelRatio)
+
+    const pixelCount = width * height
+
+    const renderScale = pixelCount > maxPixelCount ? Math.sqrt(maxPixelCount/pixelCount) : 1
+
+    width = Math.floor(width*renderScale)
+    height = Math.floor(height*renderScale)
+
+    const needResize = canvas.width !== width || canvas.height !== height
+    if(needResize){
+        renderer.setSize(width, height, false)
+    }
+    return needResize
+}
+
+
+const cube = new THREE.Mesh(
 new THREE.BoxGeometry(1, 1, 1),
 new THREE.MeshBasicMaterial({ color: 'red' }));
 
+scene.add(cube);
 
-gsap.to(cube1.position,{
-    y:2,
-    duration:1,
-    repeat:-1,
-    yoyo:true
-})
-
-group1.add(cube1);
-group1.add(cube2);
-
-
-cube2.position.x = -2
-
-
-const camera = new THREE.PerspectiveCamera(75, 900/600);
+const camera = new THREE.PerspectiveCamera(75,
+    canvasElement.clientWidth/canvasElement.clientHeight);
 camera.position.z = 5;
 scene.add(camera);
 
@@ -42,10 +67,25 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvasElement
 });
 
-renderer.setSize(900, 600);
+
+gsap.to(cube.rotation,{
+    y:Math.PI * 2,
+    z:Math.PI * 2,
+    duration:10,
+    ease:"none",
+    repeat:-1
+})
 
 const animate = ()=> {
+
+    if(resizeRendererToDisplaySize(renderer)){
+        const canvas = renderer.domElement
+        camera.aspect = canvas.clientWidth / canvas.clientHeight
+        camera.updateProjectionMatrix()
+    }
+
     renderer.render(scene, camera);
+
     
     window.requestAnimationFrame(animate)
 }
